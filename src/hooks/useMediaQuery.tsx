58d5b22d3a5ee5d1d9968a394d09279fd7c83192
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2024-11-25 16:43:57 +0700

    Login

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    setMatches(mediaQuery.matches);
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, [query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
